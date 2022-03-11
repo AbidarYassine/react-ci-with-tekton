f6d43e6b51aa17214cc66a55c8b85f30ba49efbb
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-03-11 12:14:25 +0100

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './style.css';
+
+export default function Todo() {
+    return (
+        <div
+            className="container"
+        >
+            <h3>
+                Todo App with tekton cicd
+            </h3>
+            <span>Created by Abidar yassine</span>
+            <a href="https://github.com/">GitHub repo</a>
+
+        </div>
+    )
+}
